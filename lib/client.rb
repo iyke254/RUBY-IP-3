318a0f94c3afbfe477ec049cce8ab9ac439572f3
iyke <nderiony95@gmail.com>
2017-07-19 11:43:07 +0300

    fix bugs

--- lib/client.rb
+++ lib/client.rb
@@ -1,27 +1,27 @@
 class Client
-    attr_reader(:name, :stystylist_id)
+    attr_reader(:name, :stylist_id)
 
     define_method(:initialize) do |attributes|
       @name = attributes.fetch(:name)
-      @stystylist_id = attributes.fetch(:stylist_id)
+      @stylist_id = attributes.fetch(:stylist_id)
     end
 
     define_singleton_method(:all) do
-      returned_tasks = DB.exec("SELECT * FROM tasks;")
-      tasks = []
-      returned_tasks.each() do |task|
+      returned_clients = DB.exec("SELECT * FROM clients;")
+      clients = []
+      returned_clients.each() do |task|
         name = task.fetch("name")
         stylist_id = task.fetch("stylist_id").to_i() # The information comes out of the database as a string.
-        tasks.push(Task.new({:name => name, :stylist_id => stylist_id}))
+        clients.push(Task.new({:name => name, :stylist_id => stylist_id}))
       end
-      tasks
+      clients
     end
 
     define_method(:save) do
-      DB.exec("INSERT INTO tasks (name, stylist_id) VALUES ('#{@name}', #{@stylist_id});")
+      DB.exec("INSERT INTO clients (name, stylist_id) VALUES ('#{@name}', #{@stylist_id});")
     end
 
     def delete
-      DB.exec("DELETE FROM tasks WHERE stylist_id = stylist_id");
+      DB.exec("DELETE FROM clients WHERE stylist_id = stylist_id");
     end
 end
