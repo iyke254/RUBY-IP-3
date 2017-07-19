496cc6c1045b4d6c8f04714cef91f096550b7b6b
iyke <nderiony95@gmail.com>
2017-07-19 11:44:11 +0300

    fix bugs

--- lib/client.rb
+++ lib/client.rb
@@ -12,7 +12,7 @@ class Client
       returned_clients.each() do |client|
         name = client.fetch("name")
         stylist_id = client.fetch("stylist_id").to_i() # The information comes out of the database as a string.
-        clients.push(Task.new({:name => name, :stylist_id => stylist_id}))
+        clients.push(Client.new({:name => name, :stylist_id => stylist_id}))
       end
       clients
     end
