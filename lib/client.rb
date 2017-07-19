bd7ad8d09946f1950362c1987e2a45ad219187bc
iyke <nderiony95@gmail.com>
2017-07-19 11:42:28 +0300

    fix bugs

--- lib/client.rb
+++ lib/client.rb
@@ -1,9 +1,9 @@
 class Client
-    attr_reader(:name, :stylist_id)
+    attr_reader(:name, :stystylist_id)
 
     define_method(:initialize) do |attributes|
       @name = attributes.fetch(:name)
-      @stylist_id = attributes.fetch(:list_id)
+      @stystylist_id = attributes.fetch(:stylist_id)
     end
 
     define_singleton_method(:all) do
@@ -11,17 +11,17 @@ class Client
       tasks = []
       returned_tasks.each() do |task|
         name = task.fetch("name")
-        list_id = task.fetch("list_id").to_i() # The information comes out of the database as a string.
-        tasks.push(Task.new({:name => name, :list_id => list_id}))
+        stylist_id = task.fetch("stylist_id").to_i() # The information comes out of the database as a string.
+        tasks.push(Task.new({:name => name, :stylist_id => stylist_id}))
       end
       tasks
     end
 
     define_method(:save) do
-      DB.exec("INSERT INTO tasks (name, list_id) VALUES ('#{@name}', #{@list_id});")
+      DB.exec("INSERT INTO tasks (name, stylist_id) VALUES ('#{@name}', #{@stylist_id});")
     end
 
     def delete
-      DB.exec("DELETE FROM tasks WHERE list_id = list_id");
+      DB.exec("DELETE FROM tasks WHERE stylist_id = stylist_id");
     end
 end
