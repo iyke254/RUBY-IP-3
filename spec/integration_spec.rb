0472e663f147a9a04c2089e118f4273bdbb256bc
iyke <nderiony95@gmail.com>
2017-07-19 11:51:19 +0300

    fix bugs

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -3,8 +3,8 @@ require('capybara/rspec')
   Capybara.app = Sinatra::Application
   set(:show_exceptions, false)
 
-  describe('adding a new list', {:type => :feature}) do
-    it('allows a user to click a list to see the tasks and details for it') do
+  describe('adding a new stylist', {:type => :feature}) do
+    it('allows a user to click a stylist to see the tasks and details for it') do
       visit('/lists/new')
       # click_button('Add New List')
       fill_in('name', :with =>'Moringaschool Work')
@@ -15,16 +15,16 @@ require('capybara/rspec')
 
   describe('viewing all of the lists', {:type => :feature}) do
     it('allows a user to see all of the lists that have been created') do
-      list = List.new({:name => 'Moringaschool Homework', :id => nil})
-      list.save()
+      stylist = List.new({:name => 'Moringaschool Homework', :id => nil})
+      stylist.save()
       visit('/')
       click_link('View All Lists')
-      expect(page).to have_content(list.name)
+      expect(page).to have_content(stylist.name)
     end
   end
 
-  describe('seeing details for a single list', {:type => :feature}) do
-    it('allows a user to click a list to see the tasks and details for it') do
+  describe('seeing details for a single stylist', {:type => :feature}) do
+    it('allows a user to click a stylist to see the tasks and details for it') do
       test_list = List.new({:name => 'School stuff', :id => nil})
       test_list.save()
       test_task = Task.new({:description => "learn SQL", :list_id => test_list.id()})
@@ -35,8 +35,8 @@ require('capybara/rspec')
     end
   end
 
-  describe('adding tasks to a list', {:type => :feature}) do
-    it('allows a user to add a task to a list') do
+  describe('adding tasks to a stylist', {:type => :feature}) do
+    it('allows a user to add a task to a stylist') do
       test_list = List.new({:name => 'School stuff', :id => nil})
       test_list.save()
       visit("/add")
